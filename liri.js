3c3712e2126242ed2be9cbe27586969b363db67f
sara romeo <sararomeo@dyn-209-2-225-80.dyn.columbia.edu>
2019-03-20 19:09:38 -0400

    spotify

--- liri.js
+++ liri.js
@@ -8,6 +8,8 @@ var Spotify = require("node-spotify-api");
 var movie = "";
 var userInput = process.argv;
 
+var parameter = process.argv[3];
+
 // console.log(userInput);
 
 for (var i= 2;i<process.argv.length;i++){
@@ -23,7 +25,7 @@ for (var i= 2;i<process.argv.length;i++){
 
 }
 
-console.log(movie);
+// console.log(movie);
 // keys.connect({
 //     id: process.env.SPOTIFY_ID,
 //     secret: process.env.SPOTIFY_SECRET
@@ -41,7 +43,7 @@ axios.get("http://www.omdbapi.com/?t=" + movie + "  &y=&apikey=trilogy").then(
 var spotify = new Spotify(keys.spotify);
 
 
-spotify.search ({type:'track', query:'All Small Things'}, function(err,data){
+spotify.search ({type:'track', query: parameter}, function(err,data){
     if (err){
         return console.log('Error:' + err);
     }
@@ -49,7 +51,12 @@ spotify.search ({type:'track', query:'All Small Things'}, function(err,data){
     // console.log(data.tracks.items[0].album.name);
     for (var i=0;i<data.tracks.items.length;i++){
 
-            console.log(data.tracks.items[i].album.name);
+            // console.log(data.tracks.items[i].album.name);
+            console.log("this is the Artist:" + data.tracks.items[i].artists[i].name);
+            console.log("this is the Album:" + data.tracks.items[i].album.name);
+            console.log( "this is the preview URL:" + data.tracks.items[i].preview_url);
+
+
 
     }
 });
